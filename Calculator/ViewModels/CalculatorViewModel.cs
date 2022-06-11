9e6118275c0d1b940f89589963288478ac0fa621
roberto2002-03-12 <67761057+roberto2002-03-12@users.noreply.github.com>
2022-06-11 18:15:16 -0500

    Agregar archivos de proyecto.

---
+++ Calculator/ViewModels/CalculatorViewModel.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Calculator.ViewModels
+{
+    public class CalculatorViewModel : ViewModelBase
+    {
+		#region Propiedades
+		int currentState = 1;
+		string mathOperator;
+
+		double firstNumber;
+		public double FirstNumber
+        {
+			get { return firstNumber; }
+			set
+            {
+				if (firstNumber != value)
+                {
+					firstNumber = value;
+					OnPropertyChanged();
+                }
+            }
+        }
+
+		double secondNumber;
+		public double SecondNumber
+        {
+			get { return secondNumber; }
+			set
+            {
+				if (secondNumber != value)
+                {
+					secondNumber = value;
+					OnPropertyChanged();
+                }
+            }
+        }
+
+		string result;
+		public string Result
+        {
+			get { return result; }
+			set
+            {
+				if (result != value)
+                {
+					result = value;
+					OnPropertyChanged();
+                }
+            }
+        }
+
+		string operation;
+		public string Operation
+        {
+			get { return operation; }
+            set
+            {
+				if (result != value)
+                {
+					result = value;
+					OnPropertyChanged();
+                }
+            }
+        }
+        #endregion
+
+        #region Comandos
+        public ICommand OnSelectNumber { protected set; get; }
+        public ICommand OnSelectOperator { protected set; get; }
+        public ICommand OnClear { protected set; get; }
+        public ICommand OnCalculate { protected set; get; }
+        #endregion
+
+        public CalculatorViewModel()
+        {
+            OnSelectNumber = new Command<string>(
+                execute: (string parameter) =>
+                {
+                    string pressed = parameter;
+
+                    if (this.Result == "0" || currentState < 0)
+                    {
+                        this.Result = "";
+                        if (currentState < 0)
+                        {
+                            currentState *= -1;
+                        }
+                    }
+
+                    this.Result += pressed;
+
+                    double number;
+                    if (double.TryParse(this.Result, out number))
+                    {
+                        this.Result = number.ToString("N0");
+                        if(currentState == 1)
+                        {
+                            firstNumber = number;
+                        }
+                        else
+                        {
+                            secondNumber = number;
+                        }
+                    }
+                });
+
+            OnClear = new Command(() =>
+            {
+                firstNumber = 0;
+                secondNumber = 0;
+                currentState = 1;
+                this.Result = "0";
+            });
+
+            OnSelectOperator = new Command<string>(
+                execute: (string parameter) =>
+                {
+                    currentState = -2;
+                    string pressed = parameter;
+                    mathOperator = pressed;
+                });
+
+            OnCalculate = new Command(() =>
+            {
+                if (currentState == 2)
+                {
+                    var result = SimpleCalculator.Calculate(firstNumber, secondNumber, mathOperator);
+                    Result = result.ToString();
+                    firstNumber = result;
+                    currentState = -1;
+                }
+            });
+
+
+		}
+
+    }
+}
---
+++ Calculator/ViewModels/ViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Calculator.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(
+            [CallerMemberName] string propertyName = null)
+        { 
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        
+    }
+}
